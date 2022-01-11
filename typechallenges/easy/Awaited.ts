316ab06f9b5eaf5dc523183c320d401da519de4a
eddie0329 <fdsa0329@naver.com>
2022-01-11 22:03:39 +0900

    :sparkles: Solve Awaited

--- typechallenges/easy/Awaited.ts
+++ typechallenges/easy/Awaited.ts
@@ -1,6 +1,9 @@
 /* _____________ Your Code Here _____________ */
 
-type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer P> ? P extends Promise<unknown> ? MyAwaited<P> : P : T;
+type MyAwaited<T extends Promise<unknown>> = 
+  T extends Promise<infer P> ? 
+  P extends Promise<unknown> ? 
+  MyAwaited<P> : P : T;
 
 /* _____________ Test Cases _____________ */
 import { Equal, Expect } from '../utils'
