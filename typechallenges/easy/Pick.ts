6d4051c170a0b29e5920876f125659e557b9b6e8
eddie0329 <fdsa0329@naver.com>
2022-01-11 21:12:37 +0900

    :sparkles: Solve Pick

--- typechallenges/easy/Pick.ts
+++ typechallenges/easy/Pick.ts
@@ -1,7 +1,7 @@
 /* _____________ Your Code Here _____________ */
 
 type MyPick<T, K extends keyof T> = {
-  [key in K]?: T[key]
+  [key in K]: T[key]
 }
 
 /* _____________ Test Cases _____________ */
