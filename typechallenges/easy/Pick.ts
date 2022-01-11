7c382060f0307207283b69ca4e6e31a52868a73e
Eddie <40482274+eddie0329@users.noreply.github.com>
2022-01-11 18:34:09 +0900

    :construction: WIP pick

---
+++ typechallenges/easy/Pick.ts
@@ -0,0 +1,29 @@
+/* _____________ Your Code Here _____________ */
+
+type MyPick<T, K> = {
+  [key in keyof K]: T[key]
+}
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from '../utils/index'
+
+type cases = [
+  Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
+  Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
+  MyPick<Todo, 'title' | 'completed' | 'invalid'>,
+]
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+interface Expected1 {
+  title: string
+}
+
+interface Expected2 {
+  title: string
+  completed: boolean
+}
