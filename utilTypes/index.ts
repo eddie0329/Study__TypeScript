7df98e65a8988a77cdae18a66da076339775de0f
Eddie <40482274+eddie0329@users.noreply.github.com>
2022-01-12 17:54:43 +0900

    :construction: WIP

--- utilTypes/index.ts
+++ utilTypes/index.ts
@@ -4,7 +4,7 @@
  *                                                                           *
 *****************************************************************************/
 
-/** Refer https://typescript-kr.github.io/pages/utility-types.html
+/** Refer: https://typescript-kr.github.io/pages/utility-types.html
  * - Partial<T>
  * - Readonly<T>
  * - Record<K,T>
@@ -29,13 +29,16 @@
 *****************************************************************************/
 
 /*________________________________Array______________________________________*/
+type Push<T extends any[], U> = [...T, U];
+type Unshift<T extends any[], U> = [U, ...T];
 
 /*________________________________Object_____________________________________*/
 type ObjectLiteral<T> = { [key in keyof T]: T[key]; }
 
+/*_______________________________Function_____________________________________*/
+
 /*_______________________________Readonly____________________________________*/
 type SelectedReadonly<T, K extends keyof T = keyof T> = Readonly<Pick<T, K>> & Omit<T, K>;
-
 type DeepReadonly<T> = {
   readonly [key in keyof T]: keyof T[key] extends never ? T[key] : DeepReadonly<T[key]>;
 }
