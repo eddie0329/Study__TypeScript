297a894010a562bf8eb72df51abb4dd1cfc8e681
Eddie <40482274+eddie0329@users.noreply.github.com>
2022-01-13 12:57:54 +0900

    :sparkles: Add Record type

--- utilTypes/index.ts
+++ utilTypes/index.ts
@@ -6,8 +6,8 @@
 
 /** Refer: https://typescript-kr.github.io/pages/utility-types.html
  * - [X] Partial<T> 
- * - [ ] Readonly<T>
- * - [ ] Record<K,T>
+ * - [X] Readonly<T>
+ * - [X] Record<K,T>
  * - [X] Pick<T,K>
  * - [X] Omit<T,K>
  * - [ ] Exclude<T,U>
@@ -36,17 +36,18 @@ type Push<T extends any[], U> = [...T, U];
 type Unshift<T extends any[], U> = [U, ...T];
 
 /*________________________________Object_____________________________________*/
-type ObjectLiteral<T> = { [K in keyof T]: T[K]; }
 // type Partial<T> = { [K inf keyof T]?: T[K]; }
 // type Pick<T, K extends keyof T> = { [P in K]: T[P]; }
 // type Omit<T, K extends string | number | symbol> = { [P in Exclude<keyof T, K>]: T[P]; }
+// type Record<K extends string | number | symbol, T> = { [P in K]: T; }
+type ObjectLiteral<T> = { [K in keyof T]: T[K]; }
 
 /*_______________________________Promise______________________________________*/
 
 /*_______________________________Function_____________________________________*/
 
 /*_______________________________Readonly____________________________________*/
-// Readonly
+// type Readonly<T> = { readonly [P in keyof T]: T[P]; }
 type SelectedReadonly<T, K extends keyof T = keyof T> = 
   Readonly<Pick<T, K>> & Omit<T, K>;
 type DeepReadonly<T> = {
