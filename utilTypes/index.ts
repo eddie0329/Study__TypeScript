b30269e87e37c5cb0c07276eedfc2cbe79f65fc8
Eddie <40482274+eddie0329@users.noreply.github.com>
2022-01-13 12:20:43 +0900

    :sparkles: Add new utiltypes

--- utilTypes/index.ts
+++ utilTypes/index.ts
@@ -5,26 +5,29 @@
 *****************************************************************************/
 
 /** Refer: https://typescript-kr.github.io/pages/utility-types.html
- * - Partial<T>
- * - Readonly<T>
- * - Record<K,T>
- * - Pick<T,K>
- * - Omit<T,K>
- * - Exclude<T,U>
- * - NonNullable<T>
- * - Parameters<T>
- * - ConstructorParameters<T>
- * - ReturnType<T>
- * - InstanceType<T>
- * - Required<T>
- * - ThisParameterType
- * - OmitThisParameter
- * - ThisType<T>
+ * - [X] Partial<T> 
+ * - [ ] Readonly<T>
+ * - [ ] Record<K,T>
+ * - [X] Pick<T,K>
+ * - [ ] Omit<T,K>
+ * - [ ] Exclude<T,U>
+ * - [ ] NonNullable<T>
+ * - [ ] Parameters<T>
+ * - [ ] ConstructorParameters<T>
+ * - [ ] ReturnType<T>
+ * - [ ] InstanceType<T>
+ * - [ ] Required<T>
+ * - [ ] ThisParameterType
+ * - [ ] OmitThisParameter
+ * - [ ] ThisType<T>
  */
 
 /*****************************************************************************
+ *                                                                           * 
  *                                                                           *
- *                           CUSTOM_UTIL_TYPES                               *
+ *                              *UTIL_TYPES*                                 *
+ *                                                                           *
+ *             Note: Global typescript util types are commented              *
  *                                                                           *
 *****************************************************************************/
 
@@ -33,12 +36,19 @@ type Push<T extends any[], U> = [...T, U];
 type Unshift<T extends any[], U> = [U, ...T];
 
 /*________________________________Object_____________________________________*/
-type ObjectLiteral<T> = { [key in keyof T]: T[key]; }
+type ObjectLiteral<T> = { [K in keyof T]: T[K]; }
+// type Partial<T> = { [K inf keyof T]?: T[K]; }
+// type Pick<T, K extends keyof T> = { [P in K]: T[P]; }
+
+/*_______________________________Promise______________________________________*/
 
 /*_______________________________Function_____________________________________*/
 
 /*_______________________________Readonly____________________________________*/
-type SelectedReadonly<T, K extends keyof T = keyof T> = Readonly<Pick<T, K>> & Omit<T, K>;
+type SelectedReadonly<T, K extends keyof T = keyof T> = 
+  Readonly<Pick<T, K>> & Omit<T, K>;
 type DeepReadonly<T> = {
-  readonly [key in keyof T]: keyof T[key] extends never ? T[key] : DeepReadonly<T[key]>;
+  readonly [key in keyof T]: keyof T[key] extends never ? 
+    T[key] : 
+    DeepReadonly<T[key]>;
 }
