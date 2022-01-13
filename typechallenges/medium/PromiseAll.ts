3a929a060dac504df0033157f0d6fe3f30644d18
Eddie <40482274+eddie0329@users.noreply.github.com>
2022-01-13 11:41:48 +0900

    :sparkles: Solve PromiseAll

--- typechallenges/medium/PromiseAll.ts
+++ typechallenges/medium/PromiseAll.ts
@@ -1,6 +1,8 @@
 /* _____________ Your Code Here _____________ */
 
-declare function PromiseAll(values: any): any;
+declare function PromiseAll<T extends any[]>(
+  values: readonly [...T]
+): Promise<{ [K in keyof T]: T[K] extends Promise<infer S> ? S : T[K] }>
 
 
 /* _____________ Test Cases _____________ */
@@ -10,7 +12,7 @@ const promiseAllTest1 = PromiseAll([1, 2, 3] as const)
 const promiseAllTest2 = PromiseAll([1, 2, Promise.resolve(3)] as const)
 const promiseAllTest3 = PromiseAll([1, 2, Promise.resolve(3)])
 
-type cases = [
+type cases =[
   Expect<Equal<typeof promiseAllTest1, Promise<[1, 2, 3]>>>,
   Expect<Equal<typeof promiseAllTest2, Promise<[1, 2, number]>>>,
   Expect<Equal<typeof promiseAllTest3, Promise<[number, number, number]>>>
