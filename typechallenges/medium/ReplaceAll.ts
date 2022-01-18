eba17a21c3cc18264600047dc6e784e0d864d2f1
Eddie <40482274+eddie0329@users.noreply.github.com>
2022-01-18 11:41:54 +0900

    :sparkles: Solve ReplaceAll

---
+++ typechallenges/medium/ReplaceAll.ts
@@ -0,0 +1,23 @@
+/* _____________ Your Code Here _____________ */
+
+type ReplaceAll<S extends string, From extends string, To extends string> = 
+From extends '' 
+    ? S 
+    : S extends `${infer F}${From}${infer T}` 
+      ? `${F}${To}${ReplaceAll<T, From, To>}` : S;
+
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from '../utils'
+
+type cases = [
+  Expect<Equal<ReplaceAll<'foobar', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<ReplaceAll<'foobar', 'bag', 'foo'>, 'foobar'>>,
+  Expect<Equal<ReplaceAll<'foobarbar', 'bar', 'foo'>, 'foofoofoo'>>,
+  Expect<Equal<ReplaceAll<'t y p e s', ' ', ''>, 'types'>>,
+  Expect<Equal<ReplaceAll<'foobarbar', '', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<ReplaceAll<'barfoo', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<ReplaceAll<'foobarfoobar', 'ob', 'b'>, 'fobarfobar'>>,
+  Expect<Equal<ReplaceAll<'foboorfoboar', 'bo', 'b'>, 'foborfobar'>>,
+  Expect<Equal<ReplaceAll<'', '', ''>, ''>>,
+]
