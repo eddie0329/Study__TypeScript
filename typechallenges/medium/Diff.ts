bb318f18db15b3cc11f13f36cb14311628707f83
Eddie <40482274+eddie0329@users.noreply.github.com>
2022-01-25 11:40:41 +0900

    :sparkles: Solve Diff

---
+++ typechallenges/medium/Diff.ts
@@ -0,0 +1,27 @@
+/* _____________ Your Code Here _____________ */
+
+type Diff<O, O1> = {
+  [P in keyof O | keyof O1 as Exclude<P, keyof O & keyof O1>]: P extends keyof O
+    ? O[P]
+    : P extends keyof O1
+    ? O1[P]
+    : never;
+};
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from "../utils";
+
+type Foo = {
+  name: string;
+  age: string;
+};
+type Bar = {
+  name: string;
+  age: string;
+  gender: number;
+};
+
+type cases = [
+  Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
+  Expect<Equal<Diff<Bar, Foo>, { gender: number }>>
+];
